86ec928eb65bfaecda303ed0b85826e3e46711f0
Merdoyovski <merdoyovski@gmail.com>
2023-07-17 14:00:49 +0300

    add hollowdb api

---
+++ pages/api/hollowdb.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: `http://localhost:8000`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export function put(
+  key,
+  encryptedSymmetricKey,
+  encryptedData,
+  accessControlConditions
+) {
+  const data = {
+    encryptedSymmetricKey: encryptedSymmetricKey,
+    encryptedData: encryptedData,
+    accessControlConditions: accessControlConditions,
+  };
+  const payload = {
+    key: key,
+    value: data,
+  };
+  console.log("payload: ", payload);
+  return axiosClient.post("/post/encrypt", payload);
+}
+
+export function get(key) {
+  return axiosClient.get("/get/" + key);
+}
